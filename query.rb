19f47582a8e74393348598888cee487dff38718b
David Harkness <nullserver@gmail.com>
2011-04-28 09:05:24 -0400

    [DH] Initial Commit

---
+++ query.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+require 'java'
+require 'endeca_navigation.jar'
+java_import 'com.endeca.navigation.ENEQueryResults'
+java_import 'com.endeca.navigation.HttpENEConnection'
+java_import 'com.endeca.navigation.UrlENEQuery'
+
+class Endeca
+  
+  def initialize(host, port)
+    @eneConn = HttpENEConnection.new(host, port);
+  end
+  
+  def query(url, perPage, offset)
+    @eneQuery = UrlENEQuery.new(url, "UTF-8");
+    puts @eneQuery.methods.sort.inspect
+    @eneQuery.setNavNumERecs(perPage);
+    @eneQuery.setNavERecsOffset(offset);
+    parse_results @eneConn.query(@eneQuery)
+  end
+  
+  private 
+    def parse_results(results)
+      nav = results.getNavigation()
+      recs = nav.getERecs()
+      hash = {}
+      recs.each do |x|
+        x.getDimValues.each do |dv|
+        dv.each do |dimloc|
+            hash[dimloc.getDimValue.getDimensionName.to_sym] = dimloc.getDimValue.getName
+          end
+        end
+      end
+      return hash
+    end
+
+end
+
+puts "usage: query.rb XXX.XXX.XXX.XXX XXX  'N=0'"   if ARGV.size != 3
+puts "                IP_ADDRESS      PORT 'QUERY'" if ARGV.size != 3
+return -1
+
+endeca = Endeca.new(ARGV[0], ARGV[1])
+hash = endeca.query(ARGV[2], 20, 0)
+
+
+
+puts hash.inspect
